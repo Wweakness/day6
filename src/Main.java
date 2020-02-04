4d88fe2a74965d7e96a2dda95b521ea3f82f4382
Wweakness <770199484@qq.com>
2020-02-04 23:54:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int d;
+        float n1;
+        float n2;
+        float n3;
+        Scanner in=new Scanner(System.in);
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        d=in.nextInt();
+
+
+    }
+}
